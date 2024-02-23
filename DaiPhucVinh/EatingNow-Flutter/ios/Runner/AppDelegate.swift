add936625473057e3fdb383f71ee18e01a90c68a
“DevwinterChau[D” <rangdong090302@gmail.com>
2024-02-23 12:53:46 +0700

    xóa class notification.dart

--- DaiPhucVinh/EatingNow-Flutter/ios/Runner/AppDelegate.swift
+++ DaiPhucVinh/EatingNow-Flutter/ios/Runner/AppDelegate.swift
@@ -1,13 +1,33 @@
 import UIKit
 import Flutter
-
+import flutter_local_notifications
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback{(registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+    }
     GeneratedPluginRegistrant.register(with: self)
+
+    if #available(iOS 10.0, *) {
+        // Set the delegate for UNUserNotificationCenter
+        UNUserNotificationCenter.current().delegate = self
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+     // Handle notification taps when the app is in the foreground.
+      // Implement this method if you want to perform custom actions when a notification is tapped.
+      func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+          // Handle notification tap here
+          print("Received notification response")
+
+          // Perform any custom actions
+
+          // Call the completion handler when done
+          completionHandler()
+      }
 }
--- DaiPhucVinh/EatingNow-Flutter/lib/main.dart
+++ DaiPhucVinh/EatingNow-Flutter/lib/main.dart
@@ -13,18 +13,12 @@ import 'package:fam/pages/store/details_store_page.dart';
 import 'package:fam/storage/locationstorage.dart';
 import 'package:fam/util/Colors.dart';
 import 'package:fam/util/app_constants.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'Middleware/AuthMiddleware.dart';
 import 'firebase_options.dart';
-
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
