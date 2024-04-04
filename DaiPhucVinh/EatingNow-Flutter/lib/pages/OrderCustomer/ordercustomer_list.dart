d04cc9a7972ee8a1fcd9ef45f94406bae3c080d8
“DevwinterChau[D” <rangdong090302@gmail.com>
2024-04-04 15:59:26 +0700

    StreamBuilder buildCategoryItem

--- DaiPhucVinh/EatingNow-Flutter/lib/pages/OrderCustomer/ordercustomer_list.dart
+++ DaiPhucVinh/EatingNow-Flutter/lib/pages/OrderCustomer/ordercustomer_list.dart
@@ -460,17 +460,16 @@ class _OrderCustomerPageState extends State<OrderCustomerPage> {
       ),
     );
   }
-
   Widget buildCategoryItem(int index, int ordertype, String status, String tabname) {
     bool isSelected = index == selectedIndex;
+    final _streamSelectedIndex = StreamController<bool>.broadcast();
+    _streamSelectedIndex.sink.add(isSelected);
     return  Padding(
       padding: EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
       child: GestureDetector(
         onTap: () {
-          setState(() {
-            selectedIndex = index;
-          });
           _initStreamOrderList(ordertype, status);
+          _streamSelectedIndex.sink.add(isSelected);
         },
         child: Container(
           decoration: BoxDecoration(
@@ -479,18 +478,52 @@ class _OrderCustomerPageState extends State<OrderCustomerPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              BigText(
-                text: tabname,
-                size: 15,
-                color: isSelected ? AppColors.mainColor : Colors.black,
-                // Change text color based on isSelected
-                maxlines: 2,
-              ),
+              StreamBuilder(stream: _streamSelectedIndex.stream, builder: (builder, snapshot){
+                return BigText(
+                  text: tabname,
+                  size: 15,
+                  color: snapshot.data! ? AppColors.mainColor : Colors.black,
+                  // Change text color based on isSelected
+                  maxlines: 2,
+                );
+              })
             ],
           ),
         ),
       ),
     );
   }
+
+  // Widget buildCategoryItem(int index, int ordertype, String status, String tabname) {
+  //   bool isSelected = index == selectedIndex;
+  //   return  Padding(
+  //     padding: EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+  //     child: GestureDetector(
+  //       onTap: () {
+  //         setState(() {
+  //           selectedIndex = index;
+  //         });
+  //         _initStreamOrderList(ordertype, status);
+  //       },
+  //       child: Container(
+  //         decoration: BoxDecoration(
+  //           borderRadius: BorderRadius.circular(5),
+  //         ),
+  //         child: Column(
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           children: [
+  //             BigText(
+  //               text: tabname,
+  //               size: 15,
+  //               color: isSelected ? AppColors.mainColor : Colors.black,
+  //               // Change text color based on isSelected
+  //               maxlines: 2,
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
 
