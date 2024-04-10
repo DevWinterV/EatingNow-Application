0c2aa8712b9d6e64de0fb5b05127d7f422997918
“DevwinterChau[D” <rangdong090302@gmail.com>
2024-04-10 15:46:16 +0700

    Cập nhật giao diện xem danh sách sản phẩm

--- DaiPhucVinh/eattingnowapp/src/components/FoodListAdmin.jsx
+++ DaiPhucVinh/eattingnowapp/src/components/FoodListAdmin.jsx
@@ -16,9 +16,11 @@ import { AiOutlineStar } from "react-icons/ai";
 import { BsBookmarkStar } from "react-icons/bs";
 
 const FoodListAdmin = ({ filter }) => {
+  const [keyword, setkeyword] = React.useState('');
   const history = useNavigate();
   const [isLoading, setIsLoading] = React.useState(false);
   const [data, setData] = React.useState([]);
+  const [dataFiller, setdataFiller] = React.useState([]);
   const [{ user }] = useStateValue();
 
   async function onViewAppearing() {
@@ -26,27 +28,54 @@ const FoodListAdmin = ({ filter }) => {
     if (filter.filter === "") {
       let response = await TakeAllFoodListByStoreId(filter);
       setData(response.data);
+      setdataFiller(response.data);
     } else {
       let response = await TakeFoodListByStoreId(filter.filter);
       setData(response.data);
+      setdataFiller(response.data);
     }
     setIsLoading(false);
   }
-
   useEffect(() => {
     onViewAppearing();
   }, [filter]);
 
+  const handleChangkeyWord = (e) => {
+    const keyword = e.target.value;
+    setkeyword(keyword);
+  };
+
+  useEffect(() => {
+    if (keyword && keyword.trim() !== "") {
+      const filteredData = data.filter(item =>
+        item.FoodName.toLowerCase().includes(keyword.toLowerCase()) ||
+        item.Description.toLowerCase().includes(keyword.toLowerCase())
+      );
+      setdataFiller(filteredData);
+    } else {
+      setdataFiller(data);
+
+    }
+  }, [keyword]);
+
   return (
-    <div className="w-full flex items-center gap-5 scroll-smooth overflow-hidden flex-wrap justify-center">
-      {isLoading ? (
+    <div>
+      <div class="search-container">
+          <label class="label">Tìm kiếm sản phẩm:</label>
+          <div class="">
+              <input id="searchInput" value={keyword} type="text" onChange={handleChangkeyWord} placeholder="Nhập tên sản phẩm, mô tả sản phẩm tìm kiếm" class="search-input" />
+          </div>
+      </div> 
+      <div className="w-full flex items-center gap-5 scroll-smooth overflow-hidden flex-wrap justify-center">
+        
+        {isLoading ? (
         <div className="text-center pt-20">
           <Loader />
         </div>
-      ) : (
+        ) : (
         <>
-          {data && data.length > 0 ? (
-            data.map((n) => (
+          {dataFiller && dataFiller.length > 0 ? (
+            dataFiller.map((n) => (
               <div
                 key={n.FoodListId}
                 className="border-4 border-orange-100 w-275 h-full min-w-[275px] md:w-200 md:min-w-[200px] bg-white shadow-2xl rounded-3xl py-2 px-4 my-4 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative"
@@ -85,15 +114,14 @@ const FoodListAdmin = ({ filter }) => {
                       >
                         <TbEdit className="text-2xl" />
                       </button>
-
                       
                       <button className={`p-1.5 text-xs font-medium uppercase tracking-wider text-white bg-${n.IsNoiBat === true ? "green-500" : "orange-700"} rounded-full bg-opacity-50`}>
                         <AiOutlineStar
                           className="text-2xl"
                           onClick={() => {
                             Swal.fire({
-                              title: n.IsNoiBat ? "Tắt món ăn nổi bật !" : "Món ăn nổi bật !",
-                              text:  n.IsNoiBat ? `Bạn muốn tắt trạng thái nổi bật của món ${n.FoodName} không ?` : `Bạn muốn đặt món ${n.FoodName} thành món ăn nổi bật ?` ,
+                              title: n.IsNoiBat ? "Tắt sản phẩm nổi bật !" : "Sản phẩm nổi bật !",
+                              text:  n.IsNoiBat ? `Bạn muốn tắt trạng thái nổi bật của ${n.FoodName} không ?` : `Bạn muốn đặt món ${n.FoodName} thành món ăn nổi bật ?` ,
                               icon: "warning",
                               showCancelButton: true,
                               confirmButtonColor: "#3085d6",
@@ -133,8 +161,8 @@ const FoodListAdmin = ({ filter }) => {
                           className="text-2xl"
                           onClick={() => {
                             Swal.fire({
-                              title: n.IsNew ? "Tắt món mới!" : "Món mới",
-                              text:  n.IsNew ? `Bạn có muốn tắt trạng thái món mới của ${n.FoodName} không ?`: `Bạn món đặt món ${n.FoodName} thành món mới ?`,
+                              title: n.IsNew ? "Tắt sản phẩm mới!" : "Sản phẩm mới",
+                              text:  n.IsNew ? `Bạn có muốn tắt trạng thái sản phẩm mới của ${n.FoodName} không ?`: `Bạn món đặt sản phẩm ${n.FoodName} thành trạng thái sản phẩm mới ?`,
                               icon: "warning",
                               showCancelButton: true,
                               confirmButtonColor: "#3085d6",
@@ -174,8 +202,8 @@ const FoodListAdmin = ({ filter }) => {
                           className="text-2xl"
                           onClick={() => {
                             Swal.fire({
-                              title: "Xóa món ăn ?",
-                              text: "Bạn muốn xóa món ăn này ra khỏi danh sách !",
+                              title: "Xóa sản phẩm ?",
+                              text: "Bạn muốn xóa sản phẩm này ra khỏi danh sách !",
                               icon: "warning",
                               showCancelButton: true,
                               confirmButtonColor: "#3085d6",
@@ -195,7 +223,7 @@ const FoodListAdmin = ({ filter }) => {
                                     } else {
                                       Swal.fire({
                                         title: "Lỗi!",
-                                        text: "Không thể xóa. Nhóm món ăn này đã tồn tại sản phẩm!",
+                                        text: "Không thể xóa. Sản phẩm này đã có đơn hàng!",
                                         icon: "error",
                                         confirmButtonText: "OK",
                                       });
@@ -217,12 +245,13 @@ const FoodListAdmin = ({ filter }) => {
             <div className="w-full flex flex-col items-center justify-center">
               <img src={NotFound} className="h-340" />
               <p className="text-xl text-headingColor font-semibold my-2">
-                Chưa có món ăn này!
+                Không có dữ liệu sản phẩm!
               </p>
             </div>
           )}
         </>
       )}
+      </div>
     </div>
   );
 };
